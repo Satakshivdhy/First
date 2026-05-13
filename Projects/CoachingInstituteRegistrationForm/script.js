c3fe67a8e929655669a1a340fbd80fe44b3aabab
Satakshivdhy <satakshivaidhy90@gmail.com>
2026-05-14 00:28:35 +0530

    Coaching institute registration form

---
+++ Projects/CoachingInstituteRegistrationForm/script.js
@@ -0,0 +1,7 @@
+document.querySelector("form").addEventListener("submit",(event)=>{
+    event.preventdefault();
+
+    const fn = document.getElementById("fullName").value;
+    const gender = document.getElementById("input[name='gender']:checked?").value
+
+})
