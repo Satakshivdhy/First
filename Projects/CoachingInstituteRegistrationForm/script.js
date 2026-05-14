3ac0e5dd77c839acb936cdf58f7067c64b84721a
Satakshivdhy <satakshivaidhy90@gmail.com>
2026-05-14 17:44:20 +0530

    updated form

--- Projects/CoachingInstituteRegistrationForm/script.js
+++ Projects/CoachingInstituteRegistrationForm/script.js
@@ -1,7 +1,20 @@
-document.querySelector("form").addEventListener("submit",(event)=>{
+//SubmitButton
+
+document.getElementById("registrationFrom").addEventListener("submit",(event)=>{
     event.preventdefault();
 
     const fn = document.getElementById("fullName").value;
-    const gender = document.getElementById("input[name='gender']:checked?").value
+    const gender = document.getElementById("input[name='gender']:checked?").value;
+    const timings = [];
+    document.querySelectorAll("input[name='batchtiming']:checked").forEach((element)=>{timings.push(element.value)});
+
+    console.log(gender);
+    console.log(timings);
+    
+    
+
 
 })
+
+
+//ResetButton
